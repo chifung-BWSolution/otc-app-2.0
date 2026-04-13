37f591f1bf4c5b66831fa707bc76da4010a953a2
Login Chi Fung Asia <chifung.login@gmail.com>
2026-04-13 06:03:21 +0000

    File changes

--- base44/functions/syncBatch1FromBubble/entry.ts
+++ base44/functions/syncBatch1FromBubble/entry.ts
@@ -39,171 +39,174 @@ async function fetchFromBubble(typeName, startCursor = 0, maxRecords = 99999) {
 }
 
 // Mapping functions for each type
+// Bubble Data API returns display names as keys (e.g. "Clock In Time", "Staff")
 function mapOT(r) {
   return {
     bubble_id: r._id,
-    remarks: r.remarks_text || "",
-    ot_hour: r.ot_hour_number || 0,
-    event_name: r.event_name_text || "",
-    reject_reason: r.reject_reason_text || "",
-    staff_id: r.staff_custom_staff || "",
-    end_date_time: r.end_date__time_date || "",
-    is_interview: r.is_interview_boolean || false,
-    start_date_time: r.start_date__time_date || "",
-    clockin_id: r.r_clcok_in_custom_clockin || "",
-    ot_type: r.n_ot_type_custom_nos_ot_type || "",
-    status: r.status_option_os_approval_status || ""
+    remarks: r["Remarks"] || "",
+    ot_hour: r["OT Hour"] || 0,
+    event_name: r["Event Name"] || "",
+    reject_reason: r["Reject Reason"] || "",
+    staff_id: r["Staff"] || "",
+    end_date_time: r["End Date& Time"] || "",
+    is_interview: r["Is Interview"] || false,
+    start_date_time: r["Start Date& Time"] || "",
+    clockin_id: r["R_Clcok-in"] || "",
+    ot_type: r["N_OT Type"] || "",
+    status: r["Status"] || ""
   };
 }
 
 function mapLeave(r) {
   return {
     bubble_id: r._id,
-    prove_url: r.prove || "",
-    remarks: r.remarks_text || "",
-    quota: r._quota_number || 0,
-    count_year: r.count_year_date || "",
-    info_tech_url: r.info_tech || "",
-    approved: r.approved_boolean || false,
-    display_name: r.display_name_text || "",
-    reject_reason: r.reject_reason_text || "",
-    send_email: r.send_email_boolean || false,
-    staff_id: r.staff_custom_staff || "",
-    end_date_time: r.end_date___time_date || "",
-    google_event_id: r.google_event_id_text || "",
-    approver_id: r.approver_custom_staff || "",
-    rejecter_id: r.rejecter_custom_staff || "",
-    start_date_time: r.start_date___time_date || "",
-    application_reason: r.application_reason_text || "",
-    interview_email: r.interview_email_boolean || false,
-    send_approval_email: r.send_approval_email_boolean || false,
-    leave_type: r.n_leave_type_custom_nos_leave_type || "",
-    leave_period: r.n_leave_period_custom_nos_leave_period || ""
+    prove_url: r["Prove"] || "",
+    remarks: r["Remarks"] || "",
+    quota: r["-Quota"] || 0,
+    count_year: r["Count Year"] || "",
+    info_tech_url: r["Info-Tech"] || "",
+    approved: r["Approved"] || false,
+    display_name: r["Display Name"] || "",
+    reject_reason: r["Reject Reason"] || "",
+    send_email: r["Send Email"] || false,
+    staff_id: r["Staff"] || "",
+    end_date_time: r["End Date & Time"] || "",
+    google_event_id: r["Google Event ID"] || "",
+    approver_id: r["Approver"] || "",
+    rejecter_id: r["Rejecter"] || "",
+    start_date_time: r["Start Date & Time"] || "",
+    application_reason: r["Application Reason"] || r["Reason for Apply"] || "",
+    interview_email: r["Interview Email"] || false,
+    send_approval_email: r["Send Approval Email"] || false,
+    leave_type: r["N_Leave Type"] || "",
+    leave_period: r["N_Leave Period"] || ""
   };
 }
 
 function mapClockin(r) {
+  const geoIn = r["Geo Location - In"];
+  const geoOut = r["Geo Location - Out"];
   return {
     bubble_id: r._id,
-    prove_out_url: r.prove || "",
-    remarks_in: r.remarks_text || "",
-    face_image_in_url: r.face_image || "",
-    prove_in_url: r.prove___in || "",
-    clockin_time: r.clockin_time_date || "",
-    ot_minutes: r.ot_minutes_number || 0,
-    remarks_out: r.remarks___out_text || "",
-    staff_id: r.staff_custom_staff || "",
-    clock_out_time: r.clock_out_time_date || "",
-    late_minutes: r.late_minutes_number || 0,
-    accuracy_in: r.accuracy___in_number || 0,
-    accuracy_out: r.accuracy___out_number || 0,
-    face_image_out_url: r.face_image___out || "",
-    face_image_file_url_in: r.face_image_file_url_text || "",
-    reason_for_no_clock: r.reason_for_late_clock_text || "",
-    request_update_end_time: r.request_update_end_time_date || "",
-    face_image_file_url_out: r.face_image_file_url___out_text || "",
-    request_update_start_time: r.request_update_start_time_date || "",
-    geo_location_in: r.geo_location_geographic_address || "",
-    geo_location_out: r.geo_location___out_geographic_address || "",
-    status_in: r.o_status_option_os_clockin_status || "",
-    status_out: r.o_status___out_option_os_clockin_status || "",
-    tags_in: r["tag___in_list_custom_nos_clock_in_tag"] || [],
-    tags_out: r["tag___out_list_custom_nos_clock_in_tag"] || [],
-    work_location_in: r.n_work_location_custom_nos_work_location || "",
-    work_location_out: r.n_work_location___out_custom_nos_work_location || "",
-    photo_approval_in: r.o_photo_approval_option_os_photo_approval || "",
-    photo_approval_out: r.o_photo_approval_out_option_os_photo_approval || "",
-    request_update_end_location: r.request_update_end_location_custom_nos_work_location || "",
-    request_update_start_location: r.request_update_start_location_custom_nos_work_location || ""
+    prove_out_url: r["Prove - Out"] || r["Prove"] || "",
+    remarks_in: r["Remarks"] || r["Remarks - In"] || "",
+    face_image_in_url: r["Face Image - In"] || "",
+    prove_in_url: r["Prove - In"] || "",
+    clockin_time: r["Clock In Time"] || "",
+    ot_minutes: r["OT Minutes Approved"] || r["OT Minutes"] || 0,
+    remarks_out: r["Remarks - Out"] || "",
+    staff_id: r["Staff"] || "",
+    clock_out_time: r["Clock Out Time"] || "",
+    late_minutes: r["Late Minutes"] || 0,
+    accuracy_in: r["Accuracy - In"] || 0,
+    accuracy_out: r["Accuracy - Out"] || 0,
+    face_image_out_url: r["Face Image - Out"] || "",
+    face_image_file_url_in: r["Face Image File URL - In"] || "",
+    reason_for_no_clock: r["Reason for No Clock"] || r["Reason for Late Clock"] || "",
+    request_update_end_time: r["Request Update End Time"] || "",
+    face_image_file_url_out: r["Face Image File URL - Out"] || "",
+    request_update_start_time: r["Request Update Start Time"] || "",
+    geo_location_in: geoIn ? (geoIn.address || `${geoIn.lat},${geoIn.lng}`) : "",
+    geo_location_out: geoOut ? (geoOut.address || `${geoOut.lat},${geoOut.lng}`) : "",
+    status_in: r["O_Status - In"] || "",
+    status_out: r["O_Status - Out"] || "",
+    tags_in: r["Tags - in"] || r["Tag - In"] || [],
+    tags_out: r["Tags - out"] || r["Tag - Out"] || [],
+    work_location_in: r["N_Work Location - In"] || r["N_Work Location"] || "",
+    work_location_out: r["N_Work Location - Out"] || "",
+    photo_approval_in: r["O_Photo Approval"] || "",
+    photo_approval_out: r["O_Photo Approval Out"] || "",
+    request_update_end_location: r["Request Update End Location"] || "",
+    request_update_start_location: r["Request Update Start Location"] || ""
   };
 }
 
 function mapManHourDate(r) {
   return {
     bubble_id: r._id,
-    report_date: r.report_date_date || "",
-    staff_id: r.staff_custom_staff || "",
-    total_work_hour: r.total_work_hour_number || 0
+    report_date: r["Report Date"] || "",
+    staff_id: r["Staff"] || "",
+    total_work_hour: r["Total Work Hour"] || 0
   };
 }
 
 function mapManHourTask(r) {
   return {
     bubble_id: r._id,
-    asana_link: r.asana_link_text || "",
-    work_hour: r.work_hour_number || 0,
-    images: r.images_list_image || [],
-    keywords: r.project_name_text || "",
-    meeting_topic: r.meeting_topic_text || "",
-    output_count: r.output_count_number || 0,
-    task_description: r.task_description_text || "",
-    task_id: r.n_task_custom_nos_task || "",
-    project_id: r.project_custom_project || "",
-    brand_id: r.n_brand_custom_nos_brand || "",
-    meeting_invite_sent: r.meeting_invite_sent_boolean || false,
-    projects: r.projects_list_custom_project || [],
-    task_type_id: r.n_task_type_custom_nos_task_type || "",
-    man_hour_date_id: r.man_hour_date_custom_man_hour_date || "",
-    meeting_participants: r.meeting_participant_list_custom_staff || [],
-    meeting_method: r.o_meeting_method_option_meeting_method || "",
-    output_unit: r.n_output___unit_custom_nos_output___unit || "",
-    work_location: r.n_work_location_custom_nos_work_location || "",
-    meeting_duration: r.o_meeting_duration_option_meeting_duration || "",
-    meeting_request_date: r.o_meeting_request_date_option_meeting_request_date || ""
+    asana_link: r["Asana Link"] || "",
+    work_hour: r["Work Hour"] || 0,
+    images: r["Images"] || [],
+    keywords: r["Keywords"] || r["Project Name"] || "",
+    meeting_topic: r["Meeting Topic"] || "",
+    output_count: r["Output Count"] || 0,
+    task_description: r["Task Description"] || "",
+    task_id: r["N_Task"] || "",
+    project_id: r["Project"] || "",
+    brand_id: r["N_Brand"] || "",
+    meeting_invite_sent: r["Meeting Invite Sent"] || false,
+    projects: r["Projects"] || [],
+    task_type_id: r["N_Task Type"] || "",
+    man_hour_date_id: r["Man Hour Date"] || "",
+    meeting_participants: r["Meeting Participant"] || [],
+    meeting_method: r["O_Meeting Method"] || "",
+    output_unit: r["N_Output & Unit"] || "",
+    work_location: r["N_Work Location"] || "",
+    meeting_duration: r["O_Meeting Duration"] || "",
+    meeting_request_date: r["O_Meeting Request Date"] || ""
   };
 }
 
 function mapProject(r) {
   return {
     bubble_id: r._id,
-    outcome: r.outcome_text || "",
-    asana_link: r.asana_link_text || "",
-    pic_id: r.pic_custom_staff || "",
-    display_name: r.display_name_text || "",
-    started_date: r.started_date_date || "",
-    bubble_api_id: r.bubble_api_id_text || "",
-    is_key_project: r.is_key_project_boolean || false,
-    estimated_income: r.estimated_income_number || 0,
-    estimated_expense: r.estimated_expense_number || 0,
-    estimated_man_hour: r.estimated_man_hour_number || 0,
-    brands: r.n_brand_list_custom_nos_brand || [],
-    collaborators: r.collaborator_list_custom_staff || [],
-    status: r.status_option_os_project_status || "",
-    teams: r.limited_to_team_list_custom_nos_team || [],
-    locations: r.o_location_list_option_os_base_location || [],
-    roles: r.limited_to_role_list_custom_nos_team_role || [],
-    sub_projects: r.sub_projects_list_custom_project || [],
-    task_types: r.n_suggested_task_types_list_custom_nos_task_type || [],
-    project_timeline: r.n_project_timeline_custom_nos_project_timeline || ""
+    outcome: r["Outcome"] || "",
+    asana_link: r["Asana Link"] || "",
+    pic_id: r["PIC"] || "",
+    display_name: r["Display Name"] || "",
+    started_date: r["Start Date"] || "",
+    bubble_api_id: r["Bubble API ID"] || "",
+    is_key_project: r["Is Key Project"] || false,
+    estimated_income: r["Estimated Income"] || 0,
+    estimated_expense: r["Estimated Expense"] || 0,
+    estimated_man_hour: r["Estimated Man Hour"] || 0,
+    brands: r["N_Brands"] || [],
+    collaborators: r["Collaborator"] || [],
+    status: r["O_Status"] || "",
+    teams: r["N_Teams"] || [],
+    locations: r["O_Location"] || [],
+    roles: r["Limited to Role"] || [],
+    sub_projects: r["Sub-Projects"] || [],
+    task_types: r["N_Task Types"] || r["N_Suggested Task Types"] || [],
+    project_timeline: r["N_Project Timeline"] || ""
   };
 }
 
 function mapStaffKPI(r) {
   return {
     bubble_id: r._id,
-    score: r.score_number || 0,
-    asana_link: r.asana_line_text || "",
-    kpi_sales: r.kpi_sales_number || 0,
-    related_file_url: r.related_file_file || "",
-    self_score: r.self_point_number || 0,
-    leader_comment: r.leader_comment_text || "",
-    box_folder: r.working_folder_text || "",
-    key_achievement: r.key_achievement_text || "",
-    project_id: r.project_custom_project || "",
-    improve_description: r.improve_description_text || "",
-    leader_suggest_score: r.leader_suggest_point_number || 0,
-    breakthrough_description: r.breakthrough_description_text || "",
-    staff_kpi_month_id: r.staff_kpi_month_custom_staff_kpi_month || ""
+    score: r["Score"] || 0,
+    asana_link: r["Asana Link"] || r["Asana Line"] || "",
+    kpi_sales: r["KPI Sales"] || 0,
+    related_file_url: r["Related File"] || "",
+    self_score: r["Self Point"] || 0,
+    leader_comment: r["Leader Comment"] || "",
+    box_folder: r["Working Folder"] || "",
+    key_achievement: r["Key Achievement"] || "",
+    project_id: r["Project"] || "",
+    improve_description: r["Improve Description"] || "",
+    leader_suggest_score: r["Leader Suggest Point"] || 0,
+    breakthrough_description: r["Breakthrough Description"] || "",
+    staff_kpi_month_id: r["Staff KPI Month"] || ""
   };
 }
 
 function mapStaffKPIMonth(r) {
   return {
     bubble_id: r._id,
-    report_month: r.report_month_date || "",
-    staff_id: r.staff_custom_staff || "",
-    company_comment: r.company_comment_text || "",
-    company_point: r.company_point_number || 0
+    report_month: r["Report Month"] || "",
+    staff_id: r["Staff"] || "",
+    company_comment: r["Company Comment"] || "",
+    company_point: r["Company Point"] || 0
   };
 }
 
